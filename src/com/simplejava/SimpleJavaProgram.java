69dbebfcd3d28977b180aaa62bde35cf3809f0c4
HardikVora007 <hardik.v@iksulaops.com>
2017-10-03 14:55:48 +0530

    Simple Java Program with Jenkins using Git

---
+++ src/com/simplejava/SimpleJavaProgram.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.simplejava;
+
+/**
+ * @author user
+ *
+ */
+public class SimpleJavaProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Simple Java Program executed");
+
+	}
+
+}
